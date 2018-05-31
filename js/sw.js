9d28b7ceac211929214e7dc9a2d660cb8aa13096
Thibault Gribaumont <Thibault@Thibaults-MacBook-Pro.local>
2018-05-31 16:11:07 +0200

    github page

--- js/sw.js
+++ js/sw.js
@@ -3,7 +3,16 @@
 //      '/',
 //      '/styles/main.css',
       'https://tgribaumont.github.io/becode-bootstrap-restaurant/',
-      'https://tgribaumont.github.io/becode-bootstrap-restaurant/findus.html'
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/menu.html',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/gallery.html',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/findus.html',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/contact.html',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/js/jquery-1.12.3.js',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/js/functions.js',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/css/bootstrap.min.css',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/css/style.css',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/images/food-949388_1280.png',
+      'https://tgribaumont.github.io/becode-bootstrap-restaurant/images/logo.png',
 //      'https://tgribaumont.github.io/becode-bootstrap-restaurant/js/functions.js'
     ];
 
